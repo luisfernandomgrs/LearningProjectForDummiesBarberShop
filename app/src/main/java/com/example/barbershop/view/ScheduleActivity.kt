bf158f49d2e3fffe6edfdf1063a7d1a48c82c0ad
Luis Fernando Machado <>
2023-04-02 23:54:27 -0300

    Implementation to: Schedule

---
+++ app/src/main/java/com/example/barbershop/view/ScheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.example.barbershop.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.barbershop.R
+
+class ScheduleActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_schedule)
+    }
+}
